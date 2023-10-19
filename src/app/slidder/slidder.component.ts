b8f6eae540238a9c00dd69d75ddcdcf6da1e3c16
unknown <10726010>
2023-10-20 03:58:25 +0530

    first commit

---
+++ src/app/slidder/slidder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slidder',
+  templateUrl: './slidder.component.html',
+  styleUrls: ['./slidder.component.css']
+})
+export class SlidderComponent {
+
+}
